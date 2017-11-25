c00da9b9b8ea5f2073416664019387f01eeb277f
Erin Shaw <erin.kimberly.shaw@gmail.com>
2017-11-24 20:48:50 -0500

    style: harry potter font

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Hogwarts_coat_of_arms_colored_with_shading.svg/1200px-Hogwarts_coat_of_arms_colored_with_shading.svg.png" id="logo" />
         </NavLink>
         <NavLink to="/students">
-          <button className="btn-lg btn-outline-warning btn-header">Wizards</button>
+          <button className="btn-lg btn-outline-warning btn-header">Vizards</button>
         </NavLink>
     </header>
   )
--- app/components/SideBar.js
+++ app/components/SideBar.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux'
 
-
-
 function SideBar() {
   return (
       <div className="main-footer">
         <NavLink to="/AddStudent">
-          <button className="btn-outline-secondary btn-lg">Add Wizard</button>
+          <button className="btn-outline-secondary btn-lg btn-footer">Add Vizard</button>
         </NavLink>
         <NavLink to="/AddCampus">
-          <button className="btn-outline-info btn-lg">Add House</button>
+          <button className="btn-outline-info btn-lg btn-footer">Add House</button>
         </NavLink>
       </div>
   )
