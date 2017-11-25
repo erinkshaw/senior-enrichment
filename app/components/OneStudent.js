b59f8162f4e635f160c10a72aef452faac9eda0d
Erin Shaw <erin.kimberly.shaw@gmail.com>
2017-11-24 23:39:49 -0500

    style: updating student form

--- app/components/OneStudent.js
+++ app/components/OneStudent.js
@@ -1,64 +1,76 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { removeStudent, putStudent, fetchStudents, getStudent } from '../store'
+import { NavLink, withRouter } from 'react-router-dom';
+import { removeStudent, putStudent, fetchStudents } from '../store'
 
 function OneStudent(props) {
   const studentId = props.match.params.studentId
   const students = props.students
   const campuses = props.campuses
-  const findStudent = (student) =>  student.id == +studentId
+  const findStudent = (student) => student.id == +studentId
   const singleStudent = students.find(findStudent)
 
-
-    return (
-    <div>
+  return (
+    <div className="one-student">
       <NavLink to={`/campuses/${singleStudent.campus.id}`}>
-      <img src={singleStudent && singleStudent.campus.imgUrl} />
+        <img src={singleStudent && singleStudent.campus.imgUrl} />
       </NavLink>
-      <h3>Edit Wizard</h3>
-            <form onSubmit={props.updateStudent}>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Wand</th>
-            <th>House           </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <button type="button" className="close" aria-label="Close" onClick={props.deleteStudent}>
-                  <span aria-hidden="true">&times;</span>
-              </button>
-            </td>
-            <td>
-              <input  defaultValue={singleStudent && singleStudent.name} name="name" />
-            </td>
-            <td>
-              <input  defaultValue={singleStudent && singleStudent.wand} name="wand" />
-            </td>
-            <td>
-              <select className="form-control houseSelect" name="houseSort" >
-                <option defaultValue value={singleStudent.campus.id}>{singleStudent && singleStudent.campus.name}</option>
-                {campuses.map( campus => {
-                  if (singleStudent && campus.name !== singleStudent.campus.name) {
-                    return <option key={campus.id} value={campus.id}>{campus.name}</option>
-                  }
-                  })}
-              </select>
-            </td>
-            <td>
-              <button type="submit" className="btn btn-outline-danger"> Submit</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-              </form>
+      <div id="edit-student">
+        <h3 style={{ fontFamily: 'LUMOS', textAlign: 'center' }}>Edit Vizard</h3>
+        <form onSubmit={props.updateStudent}>
+          <table>
+            <thead>
+              <th />
+              <th>Name</th>
+              <th>Wand</th>
+              <th>House           </th>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <button type="button" className="close" aria-label="Close" onClick={props.deleteStudent}>
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </td>
+                <td>
+                  <input className="form-control"defaultValue={singleStudent && singleStudent.name} name="name" />
+                </td>
+                <td>
+                  <input className="form-control" style={{width: '200px'}} defaultValue={singleStudent && singleStudent.wand} name="wand" />
+                </td>
+                <td>
+                  {/* <select className="form-control houseSelect" name="houseSort" >
+                    <option defaultValue value={singleStudent.campus.id}>{singleStudent && singleStudent.campus.name}</option>
+                    {campuses.map(campus => {
+                      if (singleStudent && campus.name !== singleStudent.campus.name) {
+                        return <option key={campus.id} value={campus.id}>{campus.name}</option>
+                      }
+                    })}
+                  </select> */}
+                  {/* <div className="dropdown" onSelect={function(evt){console.log(evt)}}>
+                    <button className="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {singleStudent && singleStudent.campus.name}
+                    </button>
+                    <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                    {campuses.map(campus => {
+                      if (singleStudent && campus.name !== singleStudent.campus.name) {
+                        return  <button className="dropdown-item" type="button" key={campus.id} value={campus.id}>{campus.name}</button>
+                      }
+                    })}
+                    </div>
+                  </div> */}
+                  <DropdownButton />
+                </td>
+                <td>
+                  <button type="submit" className="btn btn-outline-danger"> Submit</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </form>
+      </div>
     </div>
-    )
+  )
 }
 const mapStateToProps = function (state) {
   return {
@@ -69,24 +81,25 @@ const mapStateToProps = function (state) {
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    deleteStudent(event){
+    deleteStudent(event) {
       const studentId = ownProps.match.params.studentId
       event.preventDefault();
       dispatch(removeStudent(studentId))
       dispatch(fetchStudents())
       ownProps.history.push('/students')
     },
-    updateStudent(event){
+    updateStudent(event) {
       const id = ownProps.match.params.studentId
       const name = event.target.name.value
       const wand = event.target.wand.value
       const campusId = event.target.houseSort.value
       event.preventDefault()
-      dispatch(putStudent({id, name, wand, campusId}))
+      dispatch(putStudent({ id, name, wand, campusId }))
       dispatch(fetchStudents())
-      ownProps.history.push('/students')}
+      ownProps.history.push('/students')
+    }
   }
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(OneStudent);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(OneStudent))
